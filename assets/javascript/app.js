64192870d35fb82c11123fdd939ba9dfed60a18c
chitty <rychitty@icloud.com>
2018-11-14 19:18:31 -0600

    added a card for front page and api for gov site for austin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,4 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.FloatingActionButton.init(elems, {
       direction: 'left'
     });
+    //end nav bar
+
+    $.ajax({
+      url: "https://data.austintexas.gov/resource/nguv-n54k.json",
+      type: "GET",
+      data: {
+        "$limit" : 5000,
+        "$$app_token" : "i03YK9NGI8Vg6d6pqTTHndSeF"
+      }
+  }).done(function(data) {
+    
+    console.log('Here is your data, sir!' + data);
+  });
+
   });
